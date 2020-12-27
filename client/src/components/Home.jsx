239547c762c74c923597cee8233b247f53615b4d
Brandon Richter <firewlker21@yahoo.com>
2020-12-27 08:05:42 -0600

    added new showcase images/ prior to site ui redesign

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,19 @@
-import React from "react";
-import ProductShowcase from "./ProductShowcase";
-import Showcase from "./Showcase";
-import Footer from "./Footer/Footer";
-import WeAreSection from "./WeAreSection";
+import React from 'react'
+import ProductShowcase from './ProductShowcase'
+import Showcase from './Showcase'
+import Footer from './Footer/Footer'
+import WeAreSection from './WeAreSection'
 
 function Home() {
-   return (
-       <div>
-           <Showcase 
-    imgURL= "/images/watermark.png"
-    />
-    
-    <WeAreSection />
-    <ProductShowcase />
-    <Footer />
-       </div>
-    
-   )
-};
+  return (
+    <div>
+      <Showcase imgURL='/images/watermark2.png' />
 
-export default Home;
+      <WeAreSection />
+      <ProductShowcase />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
